4b3200b066c8161fc80066585e0d91d312025fa4
erineia.silva <erineia.silva@cnj.jus.br>
2024-12-10 17:23:49 -0300

    versão boa

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,76 +4,64 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkin;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYY");
-	
-	
-	public Reservation(Integer roomNumber, Date checkin, Date checkOut) {
-		super();
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
-		this.checkin = checkin;
+		this.checkin = checkIn;
 		this.checkOut = checkOut;
 	}
-
 
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
 
-
 	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
-
 
 	public Date getCheckin() {
 		return checkin;
 	}
 
-
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
 		long diff = checkOut.getTime() - checkin.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-		
+
 	}
-	public String updateDates(Date checkIn, Date checkOut) {
+
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return ("Reeservation dates for updates mus be future.");
+			throw new DomainException("Reeservation dates for updates mus be future.");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
-		} 
-		this.checkin = checkin;
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.checkin = checkIn;
 		this.checkOut = checkOut;
-		return null;
-				
+
 	}
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkin)
-				+ ", check-out: "
-				+ ", "
-				+ duration()
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkin) + ", check-out: " + ", " + duration()
 				+ "nights";
-				
+
 	}
-
-
-
-	
-	
-	
 
 }
